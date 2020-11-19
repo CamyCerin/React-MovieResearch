dc77afb2181e0ff4145ab9bdf17b98dda31d1a17
Camille Cerin <camille.cerin@gmail.com>
2020-11-19 09:22:17 +0100

    fixed path App Components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'moment/locale/fr';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Components/App/App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "moment/locale/fr";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
