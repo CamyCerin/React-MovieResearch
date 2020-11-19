1aa38a9051c279bf59702e711456481c91434fe7
Camille Cerin <camille.cerin@gmail.com>
2020-11-19 09:58:34 +0100

    tried fixed a href with Link

--- src/Components/Layout/Header/Header.js
+++ src/Components/Layout/Header/Header.js
@@ -1,45 +1,70 @@
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 
 function Header({ history }) {
-    
-    function getResult(e) {
-        e.preventDefault();
-        var value = e.target.querySelector('input').value;
-        
-        if (value.trim() === '') {
-            return
-        }
-        e.target.querySelector('input').value = ""
-        history.push('/results?q='+value.trim());
-    }
-    
-    function handleLink(e, link){
-        e.preventDefault()
-        history.push(link)
-    }
+  function getResult(e) {
+    e.preventDefault();
+    var value = e.target.querySelector("input").value;
 
-    return (
-      <div className="App">
-        
-        <ul>
-            <li>
-                <a href='' onClick={(e) => handleLink(e,'/')}>Home</a> | 
-                <a href='' onClick={(e) => handleLink(e,'/results?categories=popular')}>Populaires</a> |
-                <a href='' onClick={(e) => handleLink(e,'/results?categories=top_rated')}>Les mieux notés</a>|
-                <a href='' onClick={(e) => handleLink(e,'/results?categories=upcoming')}>A venir</a>
-            </li>
-            <li>
-                <form onSubmit={getResult}>
-                        <div className="row">
-                            <div className="form-group col-8">
-                                <input className="form-control" type="search" placeholder="Rechercher"/>
-                            </div>
-                        </div>
-                </form>
-            </li>
-        </ul>
-      </div>
-    );
+    if (value.trim() === "") {
+      return;
+    }
+    e.target.querySelector("input").value = "";
+    history.push("/results?q=" + value.trim());
   }
-  
+
+  function handleLink(e, link) {
+    e.preventDefault();
+    history.push(link);
+  }
+
+  return (
+    <div className="App">
+      <ul>
+        <li>
+          <Link to={"/"} onClick={(e) => handleLink(e, "/")} Prevent>
+            Home{" "}
+          </Link>{" "}
+          |
+          <Link
+            to={"/results?categories=popular"}
+            onClick={(e) => handleLink(e, "/results?categories=popular")}
+            Prevent
+          >
+            Populaires
+          </Link>
+          |
+          <Link
+            to={"/results?categories=top_rated"}
+            onClick={(e) => handleLink(e, "/results?categories=top_rated")}
+            Prevent
+          >
+            Les mieux notés
+          </Link>
+          |
+          <Link
+            to={"/results?categories=upcoming"}
+            onClick={(e) => handleLink(e, "/results?categories=upcoming")}
+            Prevent
+          >
+            A venir
+          </Link>
+        </li>
+        <li>
+          <form onSubmit={getResult}>
+            <div className="row">
+              <div className="form-group col-8">
+                <input
+                  className="form-control"
+                  type="search"
+                  placeholder="Rechercher"
+                />
+              </div>
+            </div>
+          </form>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
 export default withRouter(Header);
--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,102 +1,118 @@
-import { movies, urlApi, shuffle } from '../../../services';
-import { useEffect, useState } from 'react';
-import { withRouter } from "react-router-dom";
-import Moment from 'react-moment';
-Moment.globalLocale = 'fr';
+import { movies, urlApi, shuffle } from "../../../services";
+import { useEffect, useState } from "react";
+import { withRouter, Link } from "react-router-dom";
+import Moment from "react-moment";
+Moment.globalLocale = "fr";
 
 const categories = [
-                    {"type":"popular","name":"Populaire"},
-                    {"type":"top_rated","name":"Les mieux notés"},
-                    {"type":"upcoming","name":"A venir"}
-                ]
-let url = urlApi()
+  { type: "popular", name: "Populaire" },
+  { type: "top_rated", name: "Les mieux notés" },
+  { type: "upcoming", name: "A venir" },
+];
+let url = urlApi();
 const limit = 6;
 
 function Home({ history }) {
+  const [popular, setPopular] = useState([]);
+  const [top, setTop] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
 
-    const [popular, setPopular] = useState([])
-    const [top, setTop] = useState([])
-    const [upcoming, setUpcoming] = useState([])
-
-    useEffect(() => {
-        
-        const fetchSearch = async () => {
-            try {
-                for(var i = 0; i<categories.length; i++){
-                    getSearch(categories[i]);
-                }
-            } catch (error) {
-                // You can create an error state and set error here
-                console.log(error);
-            }
+  useEffect(() => {
+    const fetchSearch = async () => {
+      try {
+        for (var i = 0; i < categories.length; i++) {
+          getSearch(categories[i]);
         }
-        fetchSearch();
-        return () => {
-         // Do some cleanup   
-        }
-    },[categories])
+      } catch (error) {
+        // You can create an error state and set error here
+        console.log(error);
+      }
+    };
+    fetchSearch();
+    return () => {
+      // Do some cleanup
+    };
+  }, [categories]);
 
-    function handleLink(e, link){
-        e.preventDefault()
-        history.push(link)
-    }
-
-    function getSearch (param) {
-        console.log(param.type);
-        movies(param.type, 1).then(res=> {
-            shuffle(res.results)
-            let newValues = [];
-            for(let j=0; j<limit; j++){
-                newValues.push(res.results[j])
-            }
-            if(param.type === "popular"){
-                setPopular(newValues);
-            } else if(param.type === "top_rated"){
-                setTop(newValues);
-            } else if(param.type === "upcoming"){
-                setUpcoming(newValues);
-            }
-        })
-        .catch(error => console.log(error))
-    }
-
-    function giveState(type){
-        if(type === "popular"){
-            return popular;
-        } else if(type === "top_rated"){
-            return top;
-        } else if(type === "upcoming"){
-            return upcoming;
-        } else {
-            return []
-        }
-    }
-    return (
-      <div className="App">
-          {
-            categories.map((cat, index)=>
-                <section key={index}>
-                    <h3><a href='' onClick={(e) => handleLink(e,`/results?categories=${cat.type}`)}>{cat.name}</a></h3>
-                    <div className="row mb-3">
-                    {
-                        giveState(cat.type).map(item => 
-                        <a href="" onClick={(e) => {e.preventDefault();history.push(`/details/${item.id}`)}} className="ml-4 mb-3 card col-3" key={item.id}>
-                            <img className="card-img-top" src={`${url}${item.poster_path}`} alt="Card image cap" />
-                            <div className="card-body">
-                                <h5 className="card-title">{item.title}</h5>
-                                <p className="card-text">
-                                    <Moment format="DD MMMM YYYY">
-                                        {item.release_date}
-                                    </Moment></p>
-                            </div>
-                        </a>
-                    )}
-                    </div>
-                </section>
-            )
-          }
-      </div>
-    );
+  function handleLink(e, link) {
+    e.preventDefault();
+    history.push(link);
   }
-  
+
+  function getSearch(param) {
+    console.log(param.type);
+    movies(param.type, 1)
+      .then((res) => {
+        shuffle(res.results);
+        let newValues = [];
+        for (let j = 0; j < limit; j++) {
+          newValues.push(res.results[j]);
+        }
+        if (param.type === "popular") {
+          setPopular(newValues);
+        } else if (param.type === "top_rated") {
+          setTop(newValues);
+        } else if (param.type === "upcoming") {
+          setUpcoming(newValues);
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+
+  function giveState(type) {
+    if (type === "popular") {
+      return popular;
+    } else if (type === "top_rated") {
+      return top;
+    } else if (type === "upcoming") {
+      return upcoming;
+    } else {
+      return [];
+    }
+  }
+  return (
+    <div className="App">
+      {categories.map((cat, index) => (
+        <section key={index}>
+          <h3>
+            <Link
+              to={`/results?categories=${cat.type}`}
+              onClick={(e) => handleLink(e, `/results?categories=${cat.type}`)}
+              Prevent
+            >
+              {cat.name}
+            </Link>
+          </h3>
+          <div className="row mb-3">
+            {giveState(cat.type).map((item) => (
+              <Link
+                to={`/details/${item.id}`}
+                onClick={(e) => {
+                  e.preventDefault();
+                  history.push(`/details/${item.id}`);
+                }}
+                Prevent
+                className="ml-4 mb-3 card col-3"
+                key={item.id}
+              >
+                <img
+                  className="card-img-top"
+                  src={`${url}${item.poster_path}`}
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{item.title}</h5>
+                  <p className="card-text">
+                    <Moment format="DD MMMM YYYY">{item.release_date}</Moment>
+                  </p>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </section>
+      ))}
+    </div>
+  );
+}
+
 export default withRouter(Home);
